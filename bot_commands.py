5cae8ce433988d4854bc667fec27e04b6278a1fd
Dmitriy <zeifd@mail.ru>
2022-08-12 22:33:24 +0700

    Добавил экспорт

--- bot_commands.py
+++ bot_commands.py
@@ -1,33 +1,51 @@
+from tokenize import Token
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import telebot
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f'/Hello\n/add\n/exp\n/imp\n/search')
+    await update.message.reply_text(f'/help\n/add\n/exp\n/imp\n/search')
 
 async def add_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    stroka = update.message.text.split()
-    with open('data\spr.csv', 'a', encoding='utf-8') as file:
-        file.write('{},{}\n'.format(stroka[1], stroka[2]))
-    await update.message.reply_text(f'Абонент {stroka[1]} с номером телефона {stroka[2]} внесен в телефонный справочник.') 
+    try:
+        stroka = update.message.text.split()
+        with open('data\spr.csv', 'a', encoding='utf-8') as file:
+            file.write('{},{}\n'.format(stroka[1], stroka[2]))
+        await update.message.reply_text(f'Абонент {stroka[1]} с номером телефона {stroka[2]} внесен в телефонный справочник.') 
+    except Exception as e:
+        update.bot.reply_to(update.message, e)
 
-# async def exp_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     with open('data\spr.csv', 'r', encoding='utf-8') as file:
-#         f = file.read()
-#         #chat_id='5056254761'
-#         await update.message.sendDocument('data\spr.csv')
+async def exp_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    try:
+        await context.bot.send_document(chat_id,open('data\spr.csv', 'rb'))
+    except Exception as e:
+        update.bot.reply_to(update.message, e)
 
-# async def exp_command(chat_id='5056254761'):
-#     files = {'photo': open('data\spr.csv', 'rb')}
-#     URL = 'https://api.telegram.org/bot'
-#     requests.post(f'{URL}"5429784803:AAG3_FbbEOZMpiGlutRqsG-9rr5QfHAREWY"/sendPhoto?chat_id={chat_id}', files=files)
+async def imp_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    file_path = '/data/imp/'
+    print('file_id')
+    file_id = await context.bot.get_file(update.message.document.file_id)
+    print(file_id)
+    #f_id = update.message.document.file_id
+    #file_info = await context.bot.get_file(f_id)
+    # downloaded_file = context.bot.download_file(file_info.file_path)
+    downloaded_file = await context.bot.download_file(file_id, file_path)
 
+#     src = file_path + update.message.document.file_name
+#     print(src)
+#     with open(src, 'wb') as new_file:
+#         new_file.write(downloaded_file)    
+    
 
+#     # src = 'C:/Python/Project/tg_bot/files/received/' + message.document.file_name;
+#     # with open(src, 'wb') as new_file:
+#     #     new_file.write(downloaded_file)
 
-# async def imp_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
 async def search_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     msg = update.message.text.split()
--- main.py
+++ main.py
@@ -7,9 +7,10 @@ from bot_commands import *
 app = ApplicationBuilder().token("5429784803:AAG3_FbbEOZMpiGlutRqsG-9rr5QfHAREWY").build()
 print('server start')
 app.add_handler(CommandHandler("hello", hello))
-app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("help", help_command))
 app.add_handler(CommandHandler("add", add_command))
-#app.add_handler(MessageHandler("exp", exp_command))
+app.add_handler(CommandHandler("exp", exp_command))
+app.add_handler(CommandHandler("imp", imp_command))
 app.add_handler(CommandHandler("search", search_command))
 
 app.run_polling()
